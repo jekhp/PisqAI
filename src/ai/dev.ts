bf446649823380d64ab67bc25fa1c8b78765656a
HJOVER KJUIRO HUAMAN <yoverkjuiroity@gmail.com>
2026-02-01 16:21:11 +0000

    quita la api y todo lo referenciado a gemini, yo ya implementare la logi

--- src/ai/dev.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/summarize-conversation-history.ts';
-import '@/ai/flows/generate-conversation-starter.ts';
-import '@/ai/flows/analyze-sentiment-of-conversation.ts';
